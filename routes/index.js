adca3d89ea66d89b68331e13b08faed4b39461be
Mandeep Kaur <mkaur864@my.centennialcollege.ca>
2023-03-25 21:22:42 -0400

    added all pages

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,33 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var port = 3000;
+
+app.set('view engine', 'ejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 /* GET checklist page. */
 router.get('/checklist', function(req, res, next) {
-  res.render('index', { title: 'Checklist' });
+  res.render('checklist', { title: 'Checklist' });
 });
 /* GET contactUs page */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
 });
 /* GET Booking services */
 router.get('/booking', function(req, res, next) {
-  res.render('index', { title: 'Booking' });
+  res.render('booking', { title: 'Booking' });
 });
 
 
