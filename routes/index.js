80ee21c63fefa76062d0e5d32d680fad6cea1c51
kyung8 <kyung8@my.centennialcollege.ca>
2023-04-05 00:54:26 -0400

    student page updated

--- routes/index.js
+++ routes/index.js
@@ -47,11 +47,31 @@ router.post("/register", indexController.processRegisterPage);
 /* GET to perform UserLogout */
 router.get("/logout", indexController.performLogout);
 
-
 /* GET Student Dashboard page */
 router.get("/student-dashboard", (req, res, next) => {
   res.render("student/dashboard", {
     title: "Student Dashboard",
+    //name: "",
+  });
+});
+/* GET Student Calendar page */
+router.get("/student-calendar", (req, res, next) => {
+  res.render("student/calendar", {
+    title: "Student Calendar",
+    //name: "",
+  });
+});
+/* GET Student Checklist page */
+router.get("/student-checklist", (req, res, next) => {
+  res.render("student/checklist", {
+    title: "Student Checklist",
+    //name: "",
+  });
+});
+/* GET Student Contact page */
+router.get("/student-contact", (req, res, next) => {
+  res.render("student/contact", {
+    title: "Student Contact",
     //name: "",
   });
 });
--- routes/student.js
+++ routes/student.js
@@ -1,10 +1,36 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+app.set('view engine', 'ejs');
+
+// /* GET Student Dashboard page */
+// router.get("/student-dashboard", (req, res, next) => {
+//   res.render("student/dashboard", {
+//     title: "Student Dashboard",
+//     //name: "",
+//   });
+// });
+// /* GET Student Calendar page */
+// router.get("/student-calendar", (req, res, next) => {
+//   res.render("student/calendar", {
+//     title: "Student Calendar",
+//     //name: "",
+//   });
+// });
+// /* GET Student Checklist page */
+// router.get("/student-checklist", (req, res, next) => {
+//   res.render("student/checklist", {
+//     title: "Student Checklist",
+//     //name: "",
+//   });
+// });
+// /* GET Student Contact page */
+// router.get("/student-contact", (req, res, next) => {
+//   res.render("student/contact", {
+//     title: "Student Contact",
+//     //name: "",
+//   });
+// });
 
 module.exports = router;
-
